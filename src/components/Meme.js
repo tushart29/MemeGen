22559ebf7929427867f19a99eafec29df46ee0c2
Tushar Thonupunoori <tushar29t@gmail.com>
2023-07-03 17:39:25 -0700

    template

---
+++ src/components/Meme.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input
+                    type="text"
+                    placeholder="Top Text"
+                    className="form--input"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom Text"
+                    className="form--input"
+                />
+                <button
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
